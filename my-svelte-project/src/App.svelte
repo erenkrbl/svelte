4f6532cc1b72e84154733fbb86ab1028f9929c75
Joseph Abraham <ekurubal24@gmail.com>
2021-12-06 21:14:27 +0300

    Added reactive value

--- my-svelte-project/src/App.svelte
+++ my-svelte-project/src/App.svelte
@@ -1,6 +1,14 @@
 <script>
-	let name = 'Eren';
+	let name = 'Steven';
+	let surname = 'Svelte';
+	$: fullname = `${name} ${surname}`;
+	$: {
+		console.log(name);
+		console.log(surname);
+		console.log(fullname);
+	}
 	let renk = '';
+	
 
 	// const firstFunction = (event) => {
 	// 	renk = event.target.value;
@@ -8,9 +16,15 @@
 </script>
 
 <main>
-	<p style="color:{renk}">{name}</p>
-	<!-- <input type='text' on:input={firstFunction}> -->
-	<input type="text" bind:value={renk}>
+	<!-- <p style="color:{renk}">{name}</p>
+	<input type='text' on:input={firstFunction}>
+	<input type="text" bind:value={renk}> -->
+
+	<!-- <p> {name} {surname} </p> -->
+	<p> {fullname} </p>
+	<input type="text" bind:value={name}>
+	<input type="text" bind:value={surname}>
+	
 </main>
 
 <style>
